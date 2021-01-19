4501d7d485dc9dd21b85a6c8438cda3cb638e70f
MotoShin <m8o8t2o8@gmail.com>
2021-01-19 22:17:27 +0900

    pngで保存できるようにした/べた書きの部分を減らした

--- ResutlPlot.R
+++ ResutlPlot.R
@@ -12,6 +12,7 @@ datas <- function(x) {
   return(list(val_mean, val_median, val_sd, poly_x, poly_y))
 }
 
+epi_num = 2000
 alg_name = c("dqn", "dqn_softupdate", "ddqn")
 path = c("[path]",
          "[path]",
@@ -19,24 +20,46 @@ path = c("[path]",
 poly_color = c(rgb(1,0.71,0.77), rgb(0.6,1,0.6), rgb(0.2,0.6,0.8))
 line_color = c(rgb(1,0.08,0.58), rgb(0,0.8,0), rgb(0,0.2,1))
 
-plot(0, 0, type = "n", xlim=c(0, 2000), ylim=c(0, 200), xlab = "episode", ylab = "reward")
-abline(h=c(0,50,100,150,200),lty="dotted",lwd=0.5)
-abline(v=c(0,500,1000,1500,2000),lty="dotted",lwd=0.5)
+val_means <- matrix(, nrow=epi_num, ncol=length(path))
+val_medians <- matrix(, nrow=epi_num, ncol=length(path))
+val_sds <- matrix(, nrow=epi_num, ncol=length(path))
+poly_xs <- matrix(, nrow=epi_num*2, ncol=length(path))
+poly_ys <- matrix(, nrow=epi_num*2, ncol=length(path))
+
 for (i in 1:length(path)) {
   # import csv
   table = read.csv(path[i], header=TRUE)
   table <- table[, 2:21]
-  
+
+  # calculate
   result <- datas(table)
-  val_mean <- result[[1]]
-  val_median <- result[[2]]
-  val_sd <- result[[3]]
-  poly_x <- result[[4]]
-  poly_y <- result[[5]]
-  
-  # plot
-  # polygon(poly_x,poly.y,col=poly_color[i],lty=0)
-  lines(val_mean,lwd=1, col=line_color[i])
+  val_means[, i] <- unlist(result[[1]])
+  val_medians[, i] <- unlist(result[[2]])
+  val_sds[, i] <- unlist(result[[3]])
+  poly_xs[, i] <- unlist(result[[4]])
+  poly_ys[, i] <- unlist(result[[5]])
 }
 
-legend("topleft", legend = alg_name, col = line_color, lty=1)
+# plot
+png("all.png", width = 800, height = 600)
+plot(0, 0, type = "n", xlim=c(0, 2000), ylim=c(0, max(val_means) + 10), xlab = "episode", ylab = "reward")
+abline(h=seq(0, max(val_means) + 10, 50),lty="dotted",lwd=0.5)
+abline(v=seq(0, epi_num, 500),lty="dotted",lwd=0.5)
+for (i in 1:length(path)) {
+  lines(val_means[, i], lwd=1, col = line_color[i])
+}
+legend("topleft", legend = alg_name, col = line_color, lty=1, bg = "white")
+dev.off()
+
+# unit plot
+for (i in 1:length(path)) {
+  png(paste(alg_name[i], ".png", sep = ""), width = 800, height = 600)
+  temp = max(val_means[, i] + val_sds[, i])
+  plot(0, 0, type = "n", xlim = c(0, 2000), ylim = c(0, temp), xlab = "episode", ylab = "reward")
+  abline(h=seq(0, temp, 50),lty="dotted",lwd=0.5)
+  abline(v=seq(0, epi_num, 500),lty="dotted",lwd=0.5)
+  polygon(poly_xs[, i], poly_ys[, i], col = poly_color[i], lty=0)
+  lines(val_means[, i], lwd=1, col = line_color[i])
+  legend("topleft", legend = alg_name[i], col = line_color[i], lty=1, bg = "white")
+  dev.off()
+}
